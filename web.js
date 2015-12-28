ad0ca5a5292213be649ae0eb1351338ff37f34ff
Jose Vicente Orts Romero <jvortsromero@gmail.com>
2015-12-28 16:39:30 +0100

    Fixed web.js

--- web.js
+++ web.js
@@ -4,6 +4,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/app'));
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
 });
